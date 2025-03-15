ed242be4e29ab4db1a96d91a3c3c5228c1118d98
shinamon <76248816+shinamon610@users.noreply.github.com>
2025-03-15 17:54:45 +0900

    fix main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
 // -*- coding:utf-8-unix -*-
 
-mod my_lib;
+use proconio::input;
 
 fn main() {
+    input! {
+        n: usize,
+        an: [i32; n],
+    }
+    println!("{}", "Yes");
+}
+
+fn solve() {
     todo!()
+}
+
+fn to_yes_no(b: bool) -> &'static str {
+    if b {
+        "Yes"
+    } else {
+        "No"
+    }
 }
